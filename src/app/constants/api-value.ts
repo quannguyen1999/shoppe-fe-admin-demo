8bb0ad1830dd5198bab6e984fba53e6152f3a9ec
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-01-05 10:53:10 +0700

    fix wrong url

--- src/app/constants/api-value.ts
+++ src/app/constants/api-value.ts
@@ -2,4 +2,4 @@ import { environment } from "../../environments/environment";
 
 export const ACCOUNT_CREATE: string = environment.apiUrl + '/accounts/create';
 
-export const ACCOUNT_EXPORT: string = environment.apiUrl + '/accounts/export';
+export const ACCOUNT_EXPORT: string = environment.apiUrl + 'accounts/export';
